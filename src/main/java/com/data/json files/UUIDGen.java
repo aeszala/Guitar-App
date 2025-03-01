c33de565fbe317703a409ed8f48fb5ef60f2e7b2
tredaman5 <smithtre1076@gmail.com>
2025-03-01 13:43:50 -0500

    adding the Assignment stubs

--- src/main/java/com/data/json files/UUIDGen.java
+++
@@ -1,7 +0,0 @@
-﻿import java.util.UUID;
-
-public class UUIDGen {
-    public static void main(String[] args) {
-        System.out.println(UUID.randomUUID());
-    }
-}
--- src/main/java/com/model/Assignment.java
+++ src/main/java/com/model/Assignment.java
@@ -1,5 +1,82 @@
 ﻿package com.model;
 
+import java.util.Date;
+
 public class Assignment {
-    
+    private double grade;
+    private String teacherComment;
+    private String studentComment;
+    private Date dueDate;
+    private boolean complete;
+
+// Constructor
+public Assignment(double grade, String teacherComment, String studentComment, Date dueDate, boolean complete) {
+    this.grade = grade;
+    this.teacherComment = teacherComment;
+    this.studentComment = studentComment;
+    this.dueDate = dueDate;
+    this.complete = complete;
+}
+
+
+public void complete()
+{
+    this.complete = true;
+}
+
+public double grade()
+{
+    return grade;
+}
+
+public double getGrade()
+{
+    return grade;
+}
+
+public void setGrade(double grade)
+{
+    this.grade = grade;
+}
+
+public String getTeacherComment()
+{
+    return teacherComment;
+}
+
+public void setTeacherComment(String teacherComment)
+{
+    this.teacherComment = teacherComment;
+}
+
+public String getStudentComment()
+{
+    return studentComment;
+}
+
+public void setStudentComment(String studentComment)
+{
+    this.studentComment = studentComment;
+}
+
+public Date getDueDate()
+{
+    return dueDate;
+}
+
+public void setDueDate(Date dueDate)
+{
+    this.dueDate = dueDate;
+}
+
+public boolean isComplete()
+{
+    return complete;
+}
+
+public void setComplete(boolean complete)
+{
+    this.complete = complete;
+}
+
 }
