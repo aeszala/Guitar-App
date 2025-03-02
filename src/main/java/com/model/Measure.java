0ee0b14b3d11ae4b27a74c8df88eadf79f4a7a44
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:02:52 -0500

    adding methods for Measure

--- src/main/java/com/model/Measure.java
+++ src/main/java/com/model/Measure.java
@@ -1,5 +1,13 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class Measure {
-  
+    private int timeSignatureTop;
+    private int timeSignatureBottom;
+    ArrayList<Sound> notes;
+
+    public void display(){
+        
+    } 
 }
