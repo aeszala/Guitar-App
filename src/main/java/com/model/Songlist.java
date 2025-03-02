d864abf0436a52756eaa88810b933b476a6ba0f1
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:40:10 -0500

    adding methods for SongList

--- src/main/java/com/model/Songlist.java
+++ src/main/java/com/model/Songlist.java
@@ -1,5 +1,28 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class Songlist {
+    private Songlist songlist;
+    ArrayList<Song> songs;
     
+    private void SongList(){
+
+    }
+
+    public Songlist getInstance(){
+
+    }
+
+    public void addSongs(String title, String Artist, int runLengthMin, int runLengthSec, double rating, ArrayList<Genre> Genre){
+
+    }
+
+    public void getSongs(String Keyword){
+
+    }
+    
+    public void saveSongs(){
+        
+    }
 }
