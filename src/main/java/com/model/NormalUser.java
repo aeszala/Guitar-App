f4e86105fc3d645c6992e8425dc072fad64c654e
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:05:54 -0500

    adding methods for NormalUser

--- src/main/java/com/model/NormalUser.java
+++ src/main/java/com/model/NormalUser.java
@@ -1,5 +1,15 @@
 ﻿package com.model;
 
+import java.util.ArrayList;
+
 public class NormalUser {
+    private int points;
+    private int streak;
+    private int ranking;
+    ArrayList<Song> publishedSongs;
+
+    public void publishedSong(Song song){
+        
+    }
     
 }
