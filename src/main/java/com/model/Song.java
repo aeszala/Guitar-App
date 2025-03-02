6e410c10b126f1c988c7d9d9071ceed4a073eb6f
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:31:37 -0500

    adding methods for Song

--- src/main/java/com/model/Song.java
+++ src/main/java/com/model/Song.java
@@ -1,5 +1,38 @@
 package com.model;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Song {
+  private UUID id;
+  private String title;
+  private String artist;
+  private int runLengthMin;
+  private int runLengthSec;
+  ArrayList<Sound> notes;
+  private int Tempo;
+  private double rating;
+  ArrayList<Review> reviews;
+  private boolean metronomeOn;
+  ArrayList<Genre> genres;
+  private Difficulty difficulty;
+  ArrayList<Measure> measures;
+  private boolean completed;
+
+  public void playSong(){
+
+  }
+
+  public void changeTempo(int Tempo){
+
+  }
+
+  public void displayTab(){
+
+  }
+
+  public void displaySheet(){
+
+  }
   
 }
