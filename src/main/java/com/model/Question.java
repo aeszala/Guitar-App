563b69e62dbc5310152d7232bdea4045605b06c7
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:17:17 -0500

    adding methods for Question

--- src/main/java/com/model/Question.java
+++ src/main/java/com/model/Question.java
@@ -1,1 +1,15 @@
-﻿
+﻿package com.model;
+
+import java.util.ArrayList;
+
+public class Question{
+    private String question;
+    ArrayList<String> choices;
+    private int answer;
+    private int userChoice;
+    private boolean Correctness;
+
+    public boolean isCorrect(){
+        return Correctness;
+    }
+}
