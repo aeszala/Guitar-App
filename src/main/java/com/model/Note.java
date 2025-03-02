c6aa41b238c0a297143495be92bc1e8bcedfe458
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:09:55 -0500

    adding methods for Note

--- src/main/java/com/model/Note.java
+++ src/main/java/com/model/Note.java
@@ -1,5 +1,17 @@
 package com.model;
 
+import javafx.scene.control.Tab;
+
 public class Note {
+    private String type;
+    private Double length;
+    private Double pitch;
+    private int String;
+    private String fret;
+    private Double Tab;
+
+    public Double convertToTab(){
+        return Tab;
+    }
   
 }
