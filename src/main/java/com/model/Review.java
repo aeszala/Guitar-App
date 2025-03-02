630b661212e6e4249e857cb0be63917f9fcb3ceb
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:23:42 -0500

    adding methods for Review

--- src/main/java/com/model/Review.java
+++ src/main/java/com/model/Review.java
@@ -1,5 +1,11 @@
 package com.model;
 
 public class Review {
-  
+  private Double rating;
+  private User author;
+  private String comment;
+
+  public Review(double rating, String comment, User Author){
+    
+  }
 }
