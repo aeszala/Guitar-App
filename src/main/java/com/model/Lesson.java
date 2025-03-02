fb474300994c93a8b43fed251fcfbfd77e8d5eba
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:00:56 -0500

    adding methods for LessonList

--- src/main/java/com/model/Lesson.java
+++ src/main/java/com/model/Lesson.java
@@ -3,14 +3,14 @@ package com.model;
 import java.util.ArrayList;
 
 public class Lesson {
-    ArrayList<Songlist> songs;
+    ArrayList<Song> songs;
     private String Topic;
     ArrayList<Assignment> assignments;
     private double progress;
     private Boolean complete;
 
     public void complete(){
-        
+
     }
     
 }
