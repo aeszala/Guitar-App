c5a146ff10aa189530ae361d72223968869e81b2
tredaman5 <smithtre1076@gmail.com>
2025-03-02 13:19:11 -0500

    adding the ComposeAssignment stubs

--- src/main/java/com/model/ComposeAssignment.java
+++ src/main/java/com/model/ComposeAssignment.java
@@ -1,1 +1,49 @@
-﻿
+﻿package com.model;
+
+import java.util.ArrayList;
+
+public class ComposeAssignment {
+
+    private String instructions;
+    private int tempo;
+    private ArrayList<Sound> notes;
+
+    //Constuctor
+    public ComposeAssignment(String instructions, int tempo, ArrayList<Sound> notes) {
+        this.instructions = instructions;
+        this.tempo = tempo;
+        this.notes = notes;
+    }
+
+    public String getInstructions()
+    {
+        return instructions;
+    } 
+
+    public int getTempo()
+    {
+        return tempo;
+    }
+
+    public ArrayList<Sound> getNotes()
+    {
+        return notes;
+    }
+
+    public void setInstructions(String instructions)
+    {
+        this.instructions = instructions;
+    } 
+
+    public void setTempo(int tempo)
+    {
+        this.tempo = tempo;
+    }
+
+    public void setNotes(ArrayList<Sound> notes) 
+    {
+        this.notes = notes;
+    }
+}
+  
+
