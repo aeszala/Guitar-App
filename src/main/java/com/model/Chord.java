bbc9cd06c6ff445b0f2668b5b9b25e921d7acb05
tredaman5 <smithtre1076@gmail.com>
2025-03-02 12:47:18 -0500

    adding the Chord stubs

--- src/main/java/com/model/Chord.java
+++ src/main/java/com/model/Chord.java
@@ -1,5 +1,46 @@
 package com.model;
+import java.util.ArrayList;
 
 public class Chord {
-  
+  private String type;
+  private ArrayList<Note> notes;
+
+  //Constructor
+  public Chord (String type, ArrayList<Note> notes)
+  {
+    this.type = type;
+    this.notes = notes;
+  }
+
+  public void convertToTab()
+  {
+    //implementation
+  } 
+
+  public void play()
+  {
+    //implementation for playing the chord
+  }
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+
+  public ArrayList<Note> getNotes()
+  {
+    return notes;
+  }
+
+public void setNotes(ArrayList<Note> notes) 
+  {
+   this.notes = notes;
+  }
+
 }
+
