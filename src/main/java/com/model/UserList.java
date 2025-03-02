a1484a6e2de624b74c1ba5aa568303e3ea7a6533
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 15:13:20 -0500

    adding methods for UserList

--- src/main/java/com/model/UserList.java
+++ src/main/java/com/model/UserList.java
@@ -1,5 +1,33 @@
 ﻿package com.model;
 
+import java.util.ArrayList;
+
 public class UserList {
+    private UserList userList;
+    ArrayList<User> users;
+
+    public void getInstance(){
+
+    }
+
+    public boolean addUser(String username, String password, String Email){
+        return addUser(null, null, null);
+    }
+    
+    public void getUser(String username){
+
+    }
+
+    public void editUser(User user, String username, String name, String password, String email){
+
+    }
+
+    public void saveUsers(){
+
+    }
+
+    public void isMatch(User user, String password){
+        
+    }
     
 }
