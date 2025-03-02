fd3c36f9fc7f92ae786791c4743661a800764d10
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:13:41 -0500

    adding methods for PlayAssignment

--- src/main/java/com/model/PlayAssignment.java
+++ src/main/java/com/model/PlayAssignment.java
@@ -1,1 +1,18 @@
-﻿
+﻿package com.model;
+
+import java.util.ArrayList;
+
+public class PlayAssignment {
+    ArrayList<Song> songs;
+    private int Tempo;
+    ArrayList<Sound> playedNotes;
+    ArrayList<Sound> correctNotes;
+    private int totalNotes;
+    private double accuracy;
+
+    public double getAccuracy(){
+        return accuracy;
+    }
+
+    
+}
