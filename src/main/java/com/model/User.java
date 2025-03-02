f908ff046f267f59b678dfb595731a30b3c8eb5f
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 15:07:21 -0500

    adding methods for User

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,5 +1,44 @@
 ﻿package com.model;
 
+import java.util.UUID;
+import java.util.ArrayList;
+
 public class User {
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    ArrayList<Song> favoriteSongs;
+    ArrayList<Song> completedSongs;
+    ArrayList<Lesson> completedLessons;
+    ArrayList<Song> mySongs;
+    public String securityQuestion;
+    public String securityAnswer;
+    public boolean login;
+
+    public void user(){
+
+    }
+
+    public boolean login(){
+        return login;
+    }
+
+    public void logOut(){
+
+    }
+
+    public void favoriteSong(Song Song){
+
+    }
+
+    public void completeSong(Song song){
+        
+    }
+
+
+
+    
     
 }
