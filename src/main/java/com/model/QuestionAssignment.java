e097858d54a37d8ad40411e45426c3fcc00635c6
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-02 14:21:48 -0500

    adding methods for QuestionAssignment

--- src/main/java/com/model/QuestionAssignment.java
+++ src/main/java/com/model/QuestionAssignment.java
@@ -1,1 +1,11 @@
-﻿
+﻿package com.model;
+
+import java.util.ArrayList;
+
+public class QuestionAssignment {
+    ArrayList<Question> questions;
+
+    public void answerQuestion(int question, int answer){
+
+    }   
+}
