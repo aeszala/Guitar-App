964009086f621e1fd4cfdf93322eb131e1fa1ba5
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-03 19:58:45 -0500

    completed task #143

--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -1,5 +1,29 @@
 ﻿package com.model;
 
+import java.security.PublicKey;
+import java.util.ArrayList;
+
 public class Student {
-    
+    private double grade;
+    ArrayList<Assignment> assignments;
+    ArrayList<Assignment> completedAssignments;
+    ArrayList<Lesson> lessons;
+    private Teacher teacher;
+    private boolean turnedIn
+
+    public void openAssignment(Assignment assignment){
+
+    }
+
+    public boolean turnIn(Assignment assignment){
+        return turnedIn;
+    }
+
+    public void comment(Assignment assignment, String comment){
+
+    }
+
+    public double viewGrades(ArrayList<Assignment> assignments){
+        return grade;
+    }
 }
--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -17,28 +17,94 @@ public class User {
     public String securityAnswer;
     public boolean login;
 
-    public void user(){
-
+    public void user(String username, String password, String email, String name, String securityQuestion, String securityAnswer){
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.securityQuestion = securityQuestion;
+        this.securityAnswer = securityAnswer;
+        this.favoriteSongs = new ArrayList<>();
+        this.completedSongs = new ArrayList<>();
+        this.completedLessons = new ArrayList<>();
+        this.mySongs = new ArrayList<>();
     }
 
-    public boolean login(){
-        return login;
+    public boolean login(String inputUsername, String inputPassword){
+        return this.username.equals(inputUsername) && this.password.equals(inputPassword);
     }
 
     public void logOut(){
-
+        System.out.println("User " + username + " has logged out.");
     }
 
     public void favoriteSong(Song Song){
-
+        if (!favoriteSongs.contains(Song)){
+            favoriteSongs.add(Song);
+            System.out.println(Song.getTitle() + " added to favorites.");;
+        }
     }
 
     public void completeSong(Song song){
-        
+        if (!completedSongs.contains(song)) {
+            completedSongs.add(song);
+            System.out.println(song.getTitle() + " marked as completed.");
+        }
+    }
+    
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Song> getFavoriteSongs() {
+        return favoriteSongs;
+    }
+
+    public ArrayList<Song> getCompletedSongs() {
+        return completedSongs;
+    }
+
+    public ArrayList<Lesson> getCompletedLessons() {
+        return completedLessons;
+    }
+
+    public ArrayList<Song> getMySongs() {
+        return mySongs;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
     }
 
 
-
-    
-    
+    @Override
+    public String toString(){
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", favoriteSongs=" + favoriteSongs.size() +
+                ", completedSongs=" + completedSongs.size() +
+                ", completedLessons=" + completedLessons.size() +
+                ", mySongs=" + mySongs.size() +
+                '}';
+    }
 }
