8bab84179fe540f9a8000f404cac3195f2ed025a
Liamnp21 <143638148+Liamnp21@users.noreply.github.com>
2025-03-20 12:14:09 -0400

    Added setters to User class

---
+++ src/main/java/com/model/MusicAppFACADE.java
@@ -0,0 +1,15 @@
+﻿package com.model;
+
+public class MusicAppFACADE {
+    private User user;
+    private Teacher teacher;
+    
+    public void login(String username, String password){
+        user.getEmail();
+        user.getUsername();
+        user.getPassword();
+        user.getId();
+        user.getSecurityQuestion();
+        user.getSecurityAnswer();
+    }
+}
