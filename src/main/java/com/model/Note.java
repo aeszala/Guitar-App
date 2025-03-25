fee545e7153bfe7d3f5558b1467a254aa03d3591
NolanBanks <nolanbanks235@gmail.com>
2025-03-25 12:32:26 -0400

    #35 added javadoc comments

--- src/main/java/com/model/Note.java
+++ src/main/java/com/model/Note.java
@@ -1,9 +1,19 @@
+/**
+ * @author (name)
+ */
+
 package com.model;
 
 import org.json.simple.JSONObject;
 
-import javafx.scene.control.Tab;
-
+/**
+ * The {@code Note} class represents a musical note with properties such as type, length, pitch,
+ * string, and fret (for string instruments). It provides methods to manipulate the note's attributes, 
+ * convert the note to a tab representation, play the note, and convert the note to JSON.
+ * 
+ * <p>This class is a subclass of {@link Sound} and specializes in notes, defining additional properties 
+ * specific to a musical note on a string instrument.
+ */
 public class Note extends Sound {
     private String type;
     private double length;
@@ -12,6 +22,16 @@ public class Note extends Sound {
     private int fret;
     public String soundType;
     
+    /**
+     * Constructor to create a {@code Note} object with the specified properties.
+     * 
+     * @param type The type of the note (e.g., "quarter", "eighth").
+     * @param length The length of the note (duration).
+     * @param pitch The pitch of the note in Hertz.
+     * @param string The string number on the instrument (e.g., 1 for the highest string).
+     * @param fret The fret number on the instrument.
+     * @param soundType The type of sound, which is always set to "note" for this class.
+     */
     public Note(String type, double length, double pitch, int string, int fret, String soundType) {
         this.type = type;
         this.length = length;
@@ -21,59 +41,127 @@ public class Note extends Sound {
         this.soundType = "note";
     }
 
+    /**
+     * Converts the note to a string-based tab representation.
+     * 
+     * @return A string that represents the note in tab format.
+     */
     public String toTab() {
         return "String: " + string + " | Fret: " + fret + " (" + type + ", " + length + ")";
     }
 
+    /**
+     * Plays the note by printing details of the note to the console.
+     */
     public void play(){
         System.out.println("Playing note: " + type + " on string " + string + " at fret " + fret +
                 " with pitch " + pitch + "Hz and length " + length);
     }
 
+    /**
+     * Gets the type of the note.
+     * 
+     * @return The type of the note (e.g., "quarter", "eighth").
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the note.
+     * 
+     * @param type The type to set for the note (e.g., "quarter", "eighth").
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Gets the length of the note.
+     * 
+     * @return The length of the note (duration).
+     */
     public double getLength() {
         return length;
     }
 
+    /**
+     * Sets the length of the note.
+     * 
+     * @param length The length (duration) to set for the note.
+     */
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * Gets the pitch of the note in Hertz.
+     * 
+     * @return The pitch of the note in Hertz.
+     */
     public double getPitch() {
         return pitch;
     }
 
+    /**
+     * Sets the pitch of the note.
+     * 
+     * @param pitch The pitch to set for the note in Hertz.
+     */
     public void setPitch(Double pitch){
         this.pitch = pitch;
     }
 
+    /**
+     * Gets the string number on the instrument for the note.
+     * 
+     * @return The string number (e.g., 1 for the highest string).
+     */
     public int getString() { 
         return string; 
     }
 
+    /**
+     * Sets the string number on the instrument for the note.
+     * 
+     * @param string The string number to set (e.g., 1 for the highest string).
+     */
     public void setString(int string) { 
         this.string = string; 
     }
 
+    /**
+     * Gets the fret number on the instrument for the note.
+     * 
+     * @return The fret number.
+     */
     public int getFret() { 
         return fret; 
     }
 
+    /**
+     * Sets the fret number on the instrument for the note.
+     * 
+     * @param fret The fret number to set.
+     */
     public void setFret(int fret) {
          this.fret = fret; 
     }
     
+    /**
+     * Gets the type of sound (which is always "note" for this class).
+     * 
+     * @return The sound type, which is "note".
+     */
     public String getSoundType() {
         return soundType;
     }
     
+    /**
+     * Converts the note to a JSON object.
+     * 
+     * @return A {@link JSONObject} representing the note's properties.
+     */
     @Override
     public JSONObject toJson() {
         JSONObject noteObject = new JSONObject();
@@ -86,6 +174,11 @@ public class Note extends Sound {
         return noteObject;
     }
 
+    /**
+     * Returns a string representation of the note object.
+     * 
+     * @return A string representing the note's attributes.
+     */
     @Override
     public String toString() {
         return "Note {" +
@@ -97,5 +190,4 @@ public class Note extends Sound {
                 ", SoundType='" + soundType + '\'' +
                 '}';
     }
-    
 }
