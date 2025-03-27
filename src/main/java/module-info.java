8027aa2a1f29816a1197acea0571c0f78e573991
aeszala <aeszala@gmail.com>
2025-03-27 12:41:24 -0400

    Adding junit to module and pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.guitar_app_one_direction {
     requires javafx.fxml;
     requires json.simple;
     requires jfugue;
+    requires junit;
 
     opens com.guitar_app_one_direction to javafx.fxml;
     exports com.guitar_app_one_direction;
