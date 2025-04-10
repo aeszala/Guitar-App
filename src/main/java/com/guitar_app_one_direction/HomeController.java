7e6003174101edf5cd9160d37ea0ab1cf450e5e6
aeszala <aeszala@gmail.com>
2025-04-10 12:35:17 -0400

    editing styles.css file

---
+++ src/main/java/com/guitar_app_one_direction/HomeController.java
@@ -0,0 +1,12 @@
+﻿package com.guitar_app_one_direction;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
