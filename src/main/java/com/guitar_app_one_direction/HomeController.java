e0219aa26e381c36e44d927cd126346d45a9761b
NolanBanks <nolanbanks235@gmail.com>
2025-04-14 04:18:32 -0400

    #219 added welcome screen code with FavoriteSongs and CompletedSongs

--- src/main/java/com/guitar_app_one_direction/HomeController.java
+++ src/main/java/com/guitar_app_one_direction/HomeController.java
@@ -1,9 +1,52 @@
 ﻿package com.guitar_app_one_direction;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class HomeController {
+import com.model.MusicAppFACADE;
+import com.model.Song;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private Label lblWelcome;
+
+    @FXML
+    private VBox vboxFavorites;
+
+    @FXML
+    private VBox vboxCompleted;
+
+    private MusicAppFACADE facade;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        facade = new MusicAppFACADE();
+
+        // Get current logged-in user
+        String username = facade.getLoggedInUsername();
+        lblWelcome.setText("Welcome, " + username + "!");
+
+        // Load favorite songs
+        List<Song> favorites = facade.getFavoriteSongs(username);
+        for (Song song : favorites) {
+            Label songLabel = new Label(song.getTitle());
+            vboxFavorites.getChildren().add(songLabel);
+        }
+
+        // Load completed songs
+        List<Song> completed = facade.getCompletedSongs(username);
+        for (Song song : completed) {
+            Label songLabel = new Label(song.getTitle());
+            vboxCompleted.getChildren().add(songLabel);
+        }
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
